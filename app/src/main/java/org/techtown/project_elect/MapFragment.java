6fb25513fe10dcf20967aa7370ea490629241707
sol <ckawoalt@naver.com>
2019-12-15 14:07:18 +0900

    데이터 추가 및 라디오 버튼 클릭이벤트 정리 후보자들의 간략한 정보를 띄우는 major list수정

--- app/src/main/java/org/techtown/project_elect/MapFragment.java
+++ app/src/main/java/org/techtown/project_elect/MapFragment.java
@@ -1,4 +1,59 @@
 package org.techtown.project_elect;
 
-public class MapFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    static double x;
+    static double y;
+    static String lo;
+    static String inf;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    public static MapFragment newInstance(double tx, double ty, String tlo, String tinf) {
+        x = tx;
+        y = ty;
+        lo = tlo;
+        inf = tinf;
+        return new MapFragment();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng SEOUL = new LatLng(x, y);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title(lo);
+        markerOptions.snippet(inf);
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+    }
 }
